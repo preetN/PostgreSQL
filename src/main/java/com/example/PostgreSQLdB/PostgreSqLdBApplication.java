bb2de99df8813c4aa9072d4a77d266f25627d200
Antarpreet <pnarain82@gmail.com>
2024-01-07 20:50:13 +1100

    connect to PostgreSQL

---
+++ src/main/java/com/example/PostgreSQLdB/PostgreSqLdBApplication.java
@@ -0,0 +1,13 @@
+package com.example.PostgreSQLdB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqLdBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqLdBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PostgreSQLdB/PostgreSqLdBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PostgreSQLdB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqLdBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
